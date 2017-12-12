b9b78237c416f0ecd3260dc8369088b60c52b807
Matija Marić <sirSlani@gmail.com>
2017-12-12 18:28:58 +0100

    day 7 part 2

--- day07/main.py
+++ day07/main.py
@@ -20,7 +20,7 @@ def solve2(towers, root):
     weights = {}
     children = {}
 
-    regex = re.compile('^(\w+) \((\d+)\)(?: -> )?(.*)$')
+    regex = re.compile(r'^(\w+) \((\d+)\)(?: -> )?(.*)$')
     for line in towers:
         m = regex.match(line)
         parent = m.group(1)
@@ -29,6 +29,25 @@ def solve2(towers, root):
         
         weights[parent] = weight
         children[parent] = child
+
+    return balance(root, weights, children)
+
+def balance(node, weights, children):
+    pairs = [(total_weight(child, weights, children), child) for child in children[node]]
+    pairs = sorted(pairs, key=lambda x:-x[0])
+    if pairs[0][0] == pairs[1][0]:
+        return 0
+    b = balance(pairs[0][1], weights, children)
+    if b == 0:
+        return weights[pairs[0][1]] - (pairs[0][0] - pairs[1][0])
+    else:
+        return b
+
+def total_weight(node, weights, children):
+    if len(children[node]) == 0:
+        return weights[node]
+    else:
+        return weights[node] + sum([total_weight(child, weights, children) for child in children[node]])
 
 if __name__ == '__main__':
     with open('day07/input.txt') as f:
